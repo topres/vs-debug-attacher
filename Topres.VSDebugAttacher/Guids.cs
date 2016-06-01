839e48aa57e49663e56f70d26ac85c0eb7078e49
Elias Sørensen <els@hesehus.dk>
2016-06-01 12:23:13 +0200

    Fix indentation

--- Topres.VSDebugAttacher/Guids.cs
+++ Topres.VSDebugAttacher/Guids.cs
@@ -4,12 +4,12 @@ using System;
 
 namespace Topres.VSDebugAttacher
 {
-    public static class GuidList
-    {
-        public const string guidAttachToPkgString = "7C740628-D130-4EBB-BB77-3780A23AE520";
+	public static class GuidList
+	{
+		public const string guidAttachToPkgString = "7C740628-D130-4EBB-BB77-3780A23AE520";
 
-        public const string guidAttachToCmdSetString = "16e2ac5c-ec3d-4ff1-a237-11ccef54fe0f";
+		public const string guidAttachToCmdSetString = "16e2ac5c-ec3d-4ff1-a237-11ccef54fe0f";
 
-        public static readonly Guid guidAttachToCmdSet = new Guid(guidAttachToCmdSetString);
-    }
+		public static readonly Guid guidAttachToCmdSet = new Guid(guidAttachToCmdSetString);
+	}
 }
--- Topres.VSDebugAttacher/PackageCommandIdList.cs
+++ Topres.VSDebugAttacher/PackageCommandIdList.cs
@@ -2,15 +2,15 @@
 
 namespace Topres.VSDebugAttacher
 {
-    public static class PackageCommandIdList
-    {
-        public const uint AttachToIISCommandId = 0x100;
+	public static class PackageCommandIdList
+	{
+		public const uint AttachToIISCommandId = 0x100;
 
-        public const uint AttachToIISExpressCommandId = 0x101;
+		public const uint AttachToIISExpressCommandId = 0x101;
 
-        public const uint AttachToNUnitCommandId = 0x102;
-        public const uint AttachToCustomExe1 = 0x103;
-        public const uint AttachToCustomExe2 = 0x104;
-        public const uint AttachToCustomExe3 = 0x105;
-    }
+		public const uint AttachToNUnitCommandId = 0x102;
+		public const uint AttachToCustomExe1 = 0x103;
+		public const uint AttachToCustomExe2 = 0x104;
+		public const uint AttachToCustomExe3 = 0x105;
+	}
 }
